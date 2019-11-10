7fcf4709311b9051cb4b7ab0f0e6923eba2d35bb
Amina Khakimova <hakami1024@gmail.com>
2019-11-10 22:24:30 +0100

    geo_viz.py refactored

--- river_stations_map_visualisation_final_amina.py
+++ river_stations_map_visualisation_final_amina.py
@@ -1,3 +1,7 @@
+"""
+This is a helper script that visualizes river stations on map
+"""
+
 import plotly.graph_objects as go
 
 import pandas as pd
@@ -5,15 +9,19 @@ import numpy as np
 
 if __name__ == '__main__':
 
+    # reading the data
     df = pd.read_csv('river_stations.csv')
 
-        df.Nextdownstreamstation = df.Nextdownstreamstation.replace('-', -1)
-        df.Nextdownstreamstation = df.Nextdownstreamstation.astype(np.int64)
+    # fixing the column with next stations numbers, that are usually integers, if not missing.
+    # we will fill missing ones by -1
 
-    print(df.info())
+    df.Nextdownstreamstation = df.Nextdownstreamstation.replace('-', -1)
+    df.Nextdownstreamstation = df.Nextdownstreamstation.astype(np.int64)
 
+    # creating the interactive plot
     fig = go.Figure()
 
+    # plotting the lines for connected stations:
     for i in range(len(df)):
         if df[df['GRDC-No.'] == df['Nextdownstreamstation'][i]].shape[0] == 0: continue
         fig.add_trace(
@@ -28,11 +36,13 @@ if __name__ == '__main__':
                 hoverinfo='none'
             )
         )
+
+    # adding the labels for stations:
     fig.add_trace(
         go.Scattergeo(
             lon=df['Longitude(DD)'],
             lat=df['Latitude(DD)'],
-            text=df['GRDC-No.'],
+            text=df['Station']+', #'+df['GRDC-No.'].astype(str),
             marker=dict(
                 color="green"
             ),
@@ -40,9 +50,11 @@ if __name__ == '__main__':
         )
     )
 
+    # setting the title:
     fig.update_layout(
         title='River Stations',
         geo_scope='europe',
     )
 
+    # the figure is ready. The map is interactive. Please, adjust the zoom and the center of the map. Enjoy!
     fig.show()
